3a8d29ea0739ebf34198c1707d379ef8dd3f4346
csi23-iloposa@colegioaltair.net <csi23-iloposa@colegioaltair.net>
2024-05-31 14:34:35 +0200

    no message

--- src/main/webapp/js.js
+++ src/main/webapp/js.js
@@ -4,62 +4,60 @@
 var letras = []
 
 var palabraAdivinada = []
-var palabra = []
+var palabra=[]
 
 function introducirLaPalabra() {
-	palabra = prompt("Introduce la palabra a adivinar");
+	palabra = prompt ("Introduce la palabra a adivinar");
 	/*se crea un array lleno de * que sea igual al tamaño de la palabra*/
 	palabraAdivinada = palabra.split("");
 	palabraAdivinada.fill("*");
-	palabra = palabra.split("");
+	palabra=palabra.split("");
 	/*elimino todas las letras*/
 	letter = []
 	/*muestro los cuadros para que el usuario vea la longitud de la palabra*/
 	mostrar()
-
+	
 }
 function pedirLaLetra() {
 	let letra;
-	let letraNueva 
 	do
 		letra = prompt("Introduce una letra")
-		
-	/* analizar si tiene un número O si tiene varias letras O si está repetida O si está vacía*/
+		/* analizar si tiene un número O si tiene varias letras O si está repetida O si está vacía*/
 	while (Number(letra) || letra.length > 1 || repe(letra) || letra == "")
+	
 
-letraNueva = letra
-		/*añadir letra al array para que no se repita */
-		letras.push(letra);
+	/*añadir letra al array para que no se repita */
+	letras.push(letra);
 	comprobarLetras(letra);
 }
 
 function mostrar() {
-	let texto = ""
-	palabraAdivinada.forEach(function(i) {
+  let texto=""
+  palabraAdivinada.forEach(function (i) {
 		if (i != "*")
-			texto = texto + '<p>' + i + "</p>"
+			texto = texto + '<p>' +i+ "</p>"
 		else
 			texto = texto + "*"
-	})
-	texto = "<p>" + texto + "</p>"
-	document.getElementById("palabra").innerHTML = (texto)
-	document.getElementById("lugar").innerHTML = (texto)
+	 })
+	texto="<p>"+texto+"</p>"
+	 document.getElementById("palabra").innerHTML=(texto)
+	 document.getElementById("lugar").innerHTML=(texto)
 }
 
 
-function comprobarLetras(letra) {
+function comprobarLetras(letra){
 	/*letra por letra la palabra*/
-	for (i = 0; i < palabra.length; i++) {
-		if (palabra[i] == letra) {
-			alert("Has acertado");
-			palabraAdivinada[i] = letra;
+	for (i=0; i<palabra.length; i++){
+		if (palabra[i]==letra){
+			alert ("Has acertado");
+			palabraAdivinada[i]=letra;
 			mostrar()
 			/*mira si hay algun * */
-			if (!palabraAdivinada.some((value) => value == "*"))
+			if (!palabraAdivinada.some((value)=>value=="*"))
 				alert("Has ganado");
 		}
 	}
-	if (!palabra.some((value) => value == letra))
+	if (!palabra.some((value)=>value==letra))
 		alert("Has fallado")
 }
 
